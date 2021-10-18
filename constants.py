3c8da80864fb1b287404c8b07d9ecaacdce7f347
Nazar <57871748+Nazarko12@users.noreply.github.com>
2021-10-18 12:08:15 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,19 @@
+# This file is intended for the third laboratory work.
+
+# Pseudo pixel
+PSEUDO_PIXEL_SIZE = 20
+
+# Screen
+INITIAL_SCREEN_SIZE = 850
+
+# Circle
+INITIAL_FIRST_RADIUS_IN_PSEUDO_PIXELS = 20
+INITIAL_SECOND_RADIUS_IN_PSEUDO_PIXELS = 18
+
+# Colors
+RED = (237, 29, 37)
+BLUE = (64, 73, 204)
+WHITE = (255, 255, 255)
+
+# Other
+PI = 3.1415926535
